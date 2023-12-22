85333b4b750e43c48b42155e13baa31c0a330abb
Luiz Gonzaga <luizgonzaga15@gmail.com>
2023-12-22 18:38:34 -0300

    Criação da classe de notificações

---
+++ Entities/Notificacoes/Notifica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notificacoes
+{
+    public class Notifica
+    {
+        public Notifica()
+        {
+            Notificacoes = new List<Notifica>();    
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifica> Notificacoes { get; set; }
+    }
+}
