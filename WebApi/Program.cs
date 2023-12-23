b8e615f8c828fefade5ea65b92009b6d437f3a16
Luiz Gonzaga <luizgonzaga15@gmail.com>
2023-12-23 01:16:57 -0300

    Ajustando a class program

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,11 @@
 using Domain.Interfaces.Generics;
+using Domain.Interfaces.ICategoria;
+using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.ISistemaFinanceiro;
+using Domain.Interfaces.IUsuarioSistemaFinanceiro;
 using Entities.Entidades;
 using Infra.Configuracao;
+using Infra.Repositorios;
 using Infra.Repositorios.Generics;
 using Microsoft.EntityFrameworkCore;
 
@@ -22,6 +27,10 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(
     .AddEntityFrameworkStores<ContextoBase>();
 
 builder.Services.AddSingleton(typeof(InterfaceGeneric<>), typeof(RepositorioGenerics<>));
+builder.Services.AddSingleton<InterfaceCategoria,RepositorioCategoria>();
+builder.Services.AddSingleton<InterfaceDespesa,RepositorioDespesa>();
+builder.Services.AddSingleton<InterfaceSistemaFinanceiro,RepositorioSistemaFinanceiro>();
+builder.Services.AddSingleton<InterfaceUsuarioSistemaFinanceiro, RepositorioUsuarioSistemaFinanceiro>();
 
 var app = builder.Build();
 
