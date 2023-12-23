6aecec592bab2ad74c43c1bdb83c04434ccf67e0
Luiz Gonzaga <luizgonzaga15@gmail.com>
2023-12-22 23:41:36 -0300

    Finalização da repositorio generico

--- Infra/Repositorios/Generics/RepoitoryGenerics.cs
+++ Infra/Repositorios/Generics/RepoitoryGenerics.cs
@@ -1,32 +1,57 @@
 ﻿using Domain.Interfaces.Generics;
+using Infra.Configuracao;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Repositorios.Generics
 {
     public class RepoitoryGenerics<T> : InterfaceGeneric<T> where T : class
     {
+        private readonly DbContextOptions<ContextoBase> _optionsBuilder;
+        public RepoitoryGenerics()
+        {
+            _optionsBuilder = new DbContextOptions<ContextoBase>();
+        }
         public async Task Add(T objeto)
         {
-            throw new NotImplementedException();
+            using (var data = new ContextoBase(_optionsBuilder))
+            {
+                await data.Set<T>().AddAsync(objeto);
+                await data.SaveChangesAsync();
+            }
         }
 
         public async Task Delete(T objeto)
         {
-            throw new NotImplementedException();
+            using (var data = new ContextoBase(_optionsBuilder))
+            {
+                data.Set<T>().Remove(objeto);
+                await data.SaveChangesAsync();
+            }
         }
 
         public async Task<T> GetEntityById(int id)
         {
-            throw new NotImplementedException();
+            using (var data = new ContextoBase(_optionsBuilder))
+            {
+                return await data.Set<T>().FindAsync(id);
+            }
         }
 
         public async Task<List<T>> List()
         {
-            throw new NotImplementedException();
+            using (var data = new ContextoBase(_optionsBuilder))
+            {
+              return await data.Set<T>().ToListAsync();
+            }
         }
 
         public async Task Update(T objeto)
         {
-            throw new NotImplementedException();
+            using (var data = new ContextoBase(_optionsBuilder))
+            {
+                data.Set<T>().Update(objeto);
+                await data.SaveChangesAsync();
+            }
         }
     }
 }
