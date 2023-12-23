399d3d5ab95e20dc1abe63110a90324f87331a5a
Luiz Gonzaga <luizgonzaga15@gmail.com>
2023-12-22 23:23:20 -0300

    Configurando a String de conexão para o SQL Server

---
+++ Infra/Configuracao/ContextoBase.cs
@@ -0,0 +1,40 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Configuracao
+{
+    public class ContextoBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextoBase(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<SistemaFinanceiro> SistemaFinanceiro { get; set; }
+        public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiro { get; set; }
+        public DbSet<Despesa> Despesa { get; set; }
+        public DbSet<Categoria> Categoria { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            return "Server=DESKTOP-QR5U840\\SQLEXPRESS;Database=;Trusted_Connection=True;";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+    }
+}
