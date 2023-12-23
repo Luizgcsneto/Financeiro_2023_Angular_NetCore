8a736bc8fbd54f969b5fcd6d2c3f28213ee84bae
Luiz Gonzaga <luizgonzaga15@gmail.com>
2023-12-23 00:27:45 -0300

    Configurando a class program da webapi

--- Infra/Repositorios/Generics/RepoitoryGenerics.cs
+++ Infra/Repositorios/Generics/RepoitoryGenerics.cs
@@ -1,10 +1,11 @@
 ﻿using Domain.Interfaces.Generics;
 using Infra.Configuracao;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
 
 namespace Infra.Repositorios.Generics
 {
-    public class RepoitoryGenerics<T> : InterfaceGeneric<T> where T : class
+    public class RepoitoryGenerics<T> : InterfaceGeneric<T>, IDisposable where T : class
     {
         private readonly DbContextOptions<ContextoBase> _optionsBuilder;
         public RepoitoryGenerics()
@@ -27,6 +28,33 @@ namespace Infra.Repositorios.Generics
                 data.Set<T>().Remove(objeto);
                 await data.SaveChangesAsync();
             }
+        }
+        bool _disposed = false;
+   
+        public void Dispose()
+        {
+            // Dispose of unmanaged resources.
+            Dispose(true);
+            // Suppress finalization.
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects).
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+            // TODO: set large fields to null.
+
+            _disposed = true;
         }
 
         public async Task<T> GetEntityById(int id)
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,9 @@
+using Domain.Interfaces.Generics;
+using Entities.Entidades;
+using Infra.Configuracao;
+using Infra.Repositorios.Generics;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContextoBase>(options => options
+.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConextion")));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(
+    options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ContextoBase>();
+
+builder.Services.AddSingleton(typeof(InterfaceGeneric<>), typeof(RepoitoryGenerics<>));
 
 var app = builder.Build();
 
