ff973cc9cafdeff46ac7201d5d8146a0c60454d7
Luiz Gonzaga <luizgonzaga15@gmail.com>
2023-12-27 11:50:42 -0300

    Ajustes dos controllers de usuario e sistemas e criação do controller de categorias

---
+++ WebApi/Controllers/CategoriasController.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Interfaces.ICategoria;
+using Domain.Interfaces.InterfacesServicos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly InterfaceCategoria _interfaceCategoria;
+        private ICategoriaServico _iCategoriaServico;
+        public CategoriasController(InterfaceCategoria interfaceCategoria,
+                ICategoriaServico iCategoriaServico)
+        {
+            _interfaceCategoria = interfaceCategoria;
+            _iCategoriaServico = iCategoriaServico;
+        }
+
+        [HttpGet("/api/ListarCategoriasUsuario")]
+        [Produces("application/json")]
+        public async Task<object> ListarCategoriasUsuario(string emailUsuario)
+        {
+            return await _interfaceCategoria.ListarCategoriasUsuario(emailUsuario);
+        }
+    }
+}
--- WebApi/Controllers/SistemaFinanceirosController.cs
+++ WebApi/Controllers/SistemaFinanceirosController.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Interfaces.InterfacesServicos;
 using Domain.Interfaces.ISistemaFinanceiro;
 using Entities.Entidades;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,7 @@ namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class SistemaFinanceirosController : ControllerBase
     {
         private readonly InterfaceSistemaFinanceiro _interfaceSistemaFinanceiro;
